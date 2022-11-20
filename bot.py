e174b3de63dff8ee765a216025f72493dd38c17b
jv141 <geroosterdbrood420@gmail.com>
2022-11-20 15:31:02 +0100

    teshht

--- bot.py
+++ bot.py
@@ -178,7 +178,7 @@ async def meme(ctx):
     content = get("https://meme-api.herokuapp.com/gimme").text
     data = json.loads(content,)
     meme = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
-    await ctx.reply(embed=meme)
+    await ctx.channel.send(embed=meme)
 
 ###IP
 
